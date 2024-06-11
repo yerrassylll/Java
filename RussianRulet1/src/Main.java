31df0de51a49612e5c145d7bd57783c6a32d6561
Erasyl Meiramov <erasyl.meiramov00@gmail.com>
2024-06-12 00:07:10 +0500

    RussianRuletVer1OnlyIf

---
+++ RussianRulet1/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int revolver = 2;
+
+        if(revolver == 3) {
+            System.out.println("Ты выжил");
+        } else if(revolver == 1) {
+            System.out.println("Ты умер");
+        } else if(revolver == 2) {
+            System.out.println("Ты умер");
+        } else if(revolver == 4) {
+            System.out.println("Ты умер");
+        } else if(revolver == 5) {
+            System.out.println("Ты умер");
+        }
+
+    }
+}
